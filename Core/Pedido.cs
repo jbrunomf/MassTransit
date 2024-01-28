d62ff55227737dd5603ac1a1b663444d5c6462bd
João Bruno Moura Fernandes <jbrunomf@outlook.com>
2024-01-28 19:59:40 -0400

    Add project files.

---
+++ Core/Pedido.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Pedido
+    {
+        public Guid Id { get; set; }
+        public Usuario Usuario { get; set; }
+        public DateTime DataCriacao { get; set; }
+
+        public Pedido(Guid id, Usuario usuario)
+        {
+            Id = id;
+            Usuario = usuario;
+        }
+
+        public Pedido()
+        { }
+
+
+        public override string ToString()
+            => $"Pedido Id: {Id}, Usuário: {Usuario.Nome}, Data de Criação: {DataCriacao:dd/MM/yyyy}";
+    }
+}
---
+++ Core/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Usuario
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Usuario(Guid id, string nome, string email)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+        }
+    }
+}
